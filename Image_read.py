5632b49cbf4e0cb21e2ad5f7e17287e238ef6147
Sahishnu Duvvuri <44216999+Chunky-D-Fluffy@users.noreply.github.com>
2025-01-07 15:39:30 -0600

    Update Image_read.py

--- Image_read.py
+++ Image_read.py
@@ -1,17 +1,13 @@
 import os
 from PIL import Image
 
-folder_path = r"C:\Users\sahis\Desktop\projects\Medical\png"
 
-images = []
-
-for file_name in os.listdir(folder_path):
-    if file_name.lower().endswith('.png'):
+def image_read(folder_path, images):
+    
+    for file_name in os.listdir(folder_path):
+        if file_name.lower().endswith('.png'):
         # Full path to the image file
-        file_path = os.path.join(folder_path, file_name)
+            file_path = os.path.join(folder_path, file_name)
 
         # Open the image and add to the list
-        images.append(Image.open(file_path))
-
-# Print the number of images loaded
-print(f"Loaded {len(images)} PNG images.")
+            images.append(Image.open(file_path))
